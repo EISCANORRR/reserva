30d1a319db4bedda332a7d932fbece47d06180de
nicholas <nicholas.souza@aluno.ifsp.edu.br>
2024-05-13 08:44:42 -0300

    aula 13/05

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+index (listALL): listagem de sessoes
+store (add): criar uma sessao
+show (list): quando queremos listar um UNICA sessao
+update: quando queremos alterar alguma sessao
+destroy (delete): quando queremos deletar uma sessao
+*/
+
+import User from '../models/user'
+import * as Yup from 'yup'
+
+class SessionController {
+    async store(req,res) {
+        
+    }
+}
---
+++ src/models/reserve.js
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose'
+import house from './house'
+
+const ReserveSchema = new Schema({
+    date: String,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    house: {
+        type: Schema.Types.ObjectId,
+        red: 'House'
+    }
+})
+
+export default model('Reserve', ReserveSchema)
